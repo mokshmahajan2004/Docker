cb129e4aa58b3539d8cd5e84159c2e23b1bfcc0f
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-06-30 23:57:10 +0530

    Docker Nodejs

---
+++ main.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express();
+
+const PORT=process.env.PORT || 8000
+app.listen('/',(req,res)=>{
+    return res.json({message:'Hey,I am nodejs in container'});
+});
+    app.listen(PORT,()=>console.log(`Server started on PORT:${PORT}`));
